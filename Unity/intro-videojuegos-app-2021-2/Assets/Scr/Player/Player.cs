74a2a2d6aae7311f24d76a7750f3c40cf76555ca
Wilmar Garcia <wgarciab@unal.edu.co>
2021-11-07 20:12:11 -0500

    Added solution to part 1

--- Unity/intro-videojuegos-app-2021-2/Assets/Scr/Player/Player.cs
+++ Unity/intro-videojuegos-app-2021-2/Assets/Scr/Player/Player.cs
@@ -31,18 +31,22 @@ public class Player : MonoBehaviour
         targetMovementDirection.Normalize();
         
         //Rotation: look at movement direction
-        //_targetRotation = Quaternion.LookRotation(targetMovementDirection);
-        
+        _targetRotation = Quaternion.LookRotation(targetMovementDirection);
+
+        if (_movementInput.x != 0 || _movementInput.y != 0)
+        {
+            _movementController.RotateTo( _targetRotation, _rotationSpeed );
+        }
         
         _movementController.Move( targetMovementDirection * _speed );
-        _movementController.RotateTo( _targetRotation, _rotationSpeed );
+
     }
 
     void ProcessInputs()
     {
         _movementInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
         //Process rotation
-        CalculateTargetRotation();
+        //CalculateTargetRotation();
     }
 
     void CalculateTargetRotation()
